1fefd231a8755249e830e8e0e7fd1b8b281d8893
harshbhalerao1507-coder <harshbhalerao1507@gmail.com>
2026-07-24 16:34:14 +0530

    fixed issues

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import app from "./app.js";
+
 dotenv.config();
+
 connectDB();
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on port https:
+  console.log(`Server running on port ${PORT}`);
 });
