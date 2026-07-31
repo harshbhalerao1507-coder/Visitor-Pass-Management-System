a94c7e90886b70276831db4fee444b188b087e29
harshbhalerao1507-coder <harshbhalerao1507@gmail.com>
2026-07-31 23:26:47 +0530

    Add Brevo for emails

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,30 +2,26 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (to, subject, text) => {
     const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
-});
+        host: "smtp-relay.brevo.com",
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.BREVO_USER,
+            pass: process.env.BREVO_PASS,
+        },
+    });
 
     try {
         await transporter.sendMail({
-            from: process.env.EMAIL_USER,
+            from: process.env.SENDER_EMAIL,
             to,
             subject,
             text,
         });
-        console.log("sendEmail called");
-        console.log(to, subject);
 
         console.log("Email sent successfully");
     } catch (error) {
         console.error("Email sending failed:", error);
-        throw error;
     }
 };
 
